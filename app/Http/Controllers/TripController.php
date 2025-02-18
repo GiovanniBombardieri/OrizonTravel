d7544f52e8b3b12b83a7a80ce99e41793a62e3e4
GiovanniBombardieri <g.bombardieri06@gmail.com>
2025-02-18 19:41:35 +0100

    :sparkles: (dev) update filter for trips

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -3,15 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Trip;
 
 class TripController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        // Prendiamo tutti i viaggi disponibili
+        $query = Trip::query();
+
+        // Filtro per paese
+        if ($request->has('country_id')) {
+            $query->whereHas('countries', function ($q) use ($request) {
+                $q->where('countries.id', $request->country_id);
+            });
+        }
+
+        // Filtro per numero minimo di posti disponibili
+        if ($request->has('min_seats')) {
+            $query->where('available_seats', '>=', $request->min_seats);
+        }
+
+        // Filtro per numero massimo di posti disponibili
+        if ($request->has('max_seats')) {
+            $query->where('available_seats', '<=', $request->max_seats);
+        }
+
+        return response()->json($query->with('countries')->get());
     }
 
     /**
